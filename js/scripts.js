b1b9c04c16fd1e08cd62fd360347f2a5a1e876ec
DominiqueYoumans <ddyoumans@icloud.com>
2019-10-22 21:36:44 -0700

    add functioning js with code in correct variable scope

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function(event) {
+    var whatToSay = "s/o to Akron!";
+    alert(whatToSay);
+  });
+
+  $("img#click-two").click(function(event) {
+    var whatToSay = "Hello, I'm LeBron Raymone James Sr. An American professional basketball player for the Los Angeles Lakers of the National Basketball Association.";
+    alert(whatToSay);
+  });
+});
